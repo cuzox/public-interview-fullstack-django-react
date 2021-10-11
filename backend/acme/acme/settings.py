053936978db77140b8333d946e82d4c5be6c43f2
tmarshall <timothyjmarshall@gmail.com>
2021-10-11 16:57:17 -0500

    basic django install running

--- backend/acme/acme/settings.py
+++ backend/acme/acme/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2p+3#=!#q#!b6g=su93zxxmkxk1eed1ejc!8#q!h=@&ul(6r-k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -74,9 +74,8 @@ WSGI_APPLICATION = 'acme.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+   'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
