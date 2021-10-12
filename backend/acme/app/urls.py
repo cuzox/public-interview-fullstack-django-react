07afabf7e9c4eb1b698b3c91f569577df821b7be
tmarshall <timothyjmarshall@gmail.com>
2021-10-12 13:18:28 -0500

    CRA

--- backend/acme/app/urls.py
+++ backend/acme/app/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, re_path
 from app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    re_path('.*', views.react_passthrough),
+    # path('admin/', admin.site.urls),
+    # re_path('.*', views.react_passthrough),
 ]
