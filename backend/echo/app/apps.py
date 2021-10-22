47f9129be4da55b530a9b8db6a56c352ea82e39d
Tim Marshall <tim@marshall.io>
2021-10-22 16:19:56 -0500

    remove commented-out code

--- backend/echo/app/apps.py
+++ backend/echo/app/apps.py
@@ -11,18 +11,3 @@ logger = logging.getLogger(__name__)
 class AppConfig(AppConfig):
    default_auto_field = "django.db.models.BigAutoField"
    name = "app"
-
-   # def ready(self):
-   #    from . import models
-
-   #    # init db records
-   #    logger.info('READY')
-   #    exists = models.EchoUser.objects.exists()
-   #    if not exists:
-   #       user = models.EchoUser.objects.create(first_name='Gia', last_name='Shoemaker', email='gia@site.com')
-   #       user.set_password('password')
-   #       user.save()
-
-   #       user = models.EchoUser.objects.create(first_name='Myo', last_name='Khin', email='myo@site.com')
-   #       user.set_password('password')
-   #       user.save()
